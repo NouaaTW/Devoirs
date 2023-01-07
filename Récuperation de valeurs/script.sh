58382b5cd48b666bb63a2e40dd5e5b144af8a5eb
NouaaTW <sarazinio19@gmail.com>
2023-01-07 20:03:34 +0100

    Récuperation de valeurs

--- Récuperation de valeurs/script.sh
+++ Récuperation de valeurs/script.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-# Répertoire de destination des fichiers compressés
-sav_dir="sav"
-
-# Vérifie que le répertoire de destination existe, sinon le crée
-if [ ! -d "$sav_dir" ]; then
-  mkdir "$sav_dir"
-fi
-
-# Crée un nom de fichier unique en utilisant la date et l'heure actuelles
+# Crée le fichier qui va contenir une valeur
 filename="bitcoin_value.txt"
 
-# Récupère la valeur du bitcoin en utilisant la commande curl et grep, et supprime le caractère "}" avec tr
+# Récupère la valeur du bitcoin en utilisant la commande curl et grep, et supprime le caractère inutile avec tr
 bitcoin_value=$(curl -s "https://api.coindesk.com/v1/bpi/currentprice/BTC.json" | grep -Po '"rate_float":\K[^,]+' | tr -d '}')
 
 # Formate la valeur du bitcoin avec deux chiffres après la virgule
